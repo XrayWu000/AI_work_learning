8d8ec62505b9627843a5ef0b17ec2081410806b9
henrywu100 <henrywu100@gmail.com>
2025-11-14 21:47:51 +0800

    learn some new

--- src/hello.py
+++ src/hello.py
@@ -8,3 +8,10 @@ print(s[::-1])
 print(s[:3])
 print(s[:3] * 3)
 print("this is a test for format:\n{0} {s} {c}".format('first', s = s, c = 32.2))
+ans = input("how are you?\n")
+print(ans)
+stack = [12,4,7,26]
+d = stack.pop()
+stack.append(int(input("some number: ")))
+#print(d + " " + *stack) 這是錯的
+print(d, *stack)
