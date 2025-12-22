24e09f75f2e861628c305516193927a9a6839e25
XrayWu000 <henrywu100@gmail.com>
2025-12-22 11:27:23 +0800

    basic file control

---
+++ src/class_1222_fileControl.py
@@ -0,0 +1,15 @@
+f = open("a.txt", "w", encoding="utf-8")
+f.write("Hello\nWorld!")
+f.close()
+
+with open("a.txt", "w", encoding="utf-8") as f:
+    f.write("Hello\nWorld!")
+
+f = open("a.txt", "r", encoding="utf-8")
+content = f.read()
+f.close()
+print(content)
+
+with open("a.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+print(content)
